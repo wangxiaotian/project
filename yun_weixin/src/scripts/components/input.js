38c8c22bb846eee6ecce5f1f8650fe3afbd091f2
wdyang_504 <wangdongyang@ycode.cn>
2016-09-01 10:12:11 +0800

    initweixin

---
+++ yun_weixin/src/scripts/components/input.js
@@ -0,0 +1,254 @@
+(function() {
+    /*这是另外一种写法，技术实现上很简单。操作实现上为先定义一个对象，
+    然后为这个对象扩展方法，每一个扩展的方法都是一个构造函数。此方法的好处就是
+    可以将组件的调用汇集到一个入口上，用一个入口来调用很多扩展的功能，类似于命名空间的
+    那种东西吧*/
+    /*
+     * 1、在生成dom后，最好是通过什么样的方式来获取生成的dom中的元素？才不至于与后来的存在命名冲突
+     * 2、获取组件内的dom时，不能通过获取类名或者id名来获取，这样会造成在实例化多个组件时，获取的元素会相关联，不能达到
+     * 单独组件初始化的效果。解决方法：将字符串包装成jQuery对象，用这个包装后的对象调用find（）方法获取后代对象，真的可以啊。
+     * 用$转换成jQuery对象时，有执行创建元素节点的过程;可以用class类名，重复也没问题
+     * 3、即样式问题，该怎么写，是写入dom中，还是写类名，在外部文件中引入样式。
+     * 这个问题写到顶级作用域里
+     * 4、为什么要采用replaceWith的方法？这种方法可以使得在dom里边配置参数，并且替换之后不留痕迹
+     */
+    // 文本输入组件
+    var TYinput = {};
+    (function() {
+        /*
+         * 此组件即是简单的操作dom，没有涉及逻辑，没有涉及与服务器交互，很简单
+         * 只封装了dom，配置选项来改变内容和基本的样式
+         */
+        // 带lable标签的文本输入框
+        var Text = TYinput.Text = function() {
+
+        }
+        Text.prototype.configuration = {
+            // 容器
+            container: '',
+            // 默认值
+            placeholder: '',
+            // 文本框长度值
+            length: 9,
+            // lable长度值
+            labelLength: 3,
+            // 要有一个name属性，在调用jQuery插件进行验证时会用到
+            name : ''
+        }
+        Text.prototype.init = function(options) {
+            var self = this;
+            $.extend(true, this, self.configuration, options);
+            self.getValue();
+            self.createDom();
+            self.placeDom();
+        }
+        Text.prototype.getValue = function() {
+                var self = this;
+                self.container = $(self.container);
+                self.label_value = self.container.data('alias');
+                self.placeholder_value = self.placeholder;
+            }
+            // 简单的组件就不需要dom外架了
+        Text.prototype.createDom = function() {
+                var self = this;
+                // 水平表单
+                self.str = '<div class = "form-group">\
+                            <label class = "control-label col-xs-' + self.labelLength + '">' + self.label_value + '</label>\
+                            <div class = "col-xs-' + self.length + '">\
+                                <input class = "form-control" type = "text" placeholder = "' + self.placeholder_value + '" name = "' + self.name + '">\
+                            </div>\
+                       </div>'
+
+            }
+            // 之前的方式大多是在容器内插入dom元素append()方法，此处是用内容代替容器元素replaceWidth()方法
+        Text.prototype.placeDom = function() {
+            var self = this;
+            self.container.replaceWith(self.str);
+        }
+    })();
+    // 文本域输入组件
+    (function() {
+        /*
+         * 此组件与input组件相同，主要的部分还是生成dom,根据配置项来改变dom的一些样式或者结构，
+         * 将css样式值作为入口来进行配置操作样式的。另外，附加了一点小逻辑，即验证字符数的输入。
+         * 此处牵扯到一个问题，即样式问题，该怎么写，是写入dom中，还是写类名，在外部文件中引入样式。
+         * 这个问题写到顶级作用域里
+         */
+        var Textarea = TYinput.Textarea = function() {
+
+        }
+        Textarea.prototype.configuration = {
+            container: '',
+            height: 100,
+            alias: '',
+            labelLength: 3,
+            length: 9,
+            maxlength: 500,
+            name: ''
+        }
+        Textarea.prototype.init = function(options) {
+            var self = this;
+
+            $.extend(true, this, self.configuration, options);
+            self.getValue();
+            self.createDom();
+            self.initStatus();
+            self.computeNumber();
+        }
+        Textarea.prototype.getValue = function() {
+            var self = this;
+            self.container = $(self.container);
+        }
+        Textarea.prototype.createDom = function() {
+            var self = this;
+            var str = '<div class = "form-group" style = "">\
+                            <label style = "" class = "control-label col-xs-' + self.labelLength + '">' + self.alias + '</label>\
+                            <div class = "col-xs-' + self.length + '">\
+                                <textarea id = "inp" class = "form-control" style = "height:' + self.height + 'px" name = "' + self.name + '"></textarea>\
+                                <p id = "war1" style = "color:red">还剩<span class = "num1"></span>个字符，最多只能输入<span class = "num2"></span>个字符</p>\
+                                <p id = "war1_space" style = "height:20px;"></p>\
+                            </div>\
+                        </div>'
+                // 这个地方的获取元素，安邦里边是把字符串变为了dom对象，然后在获取
+            var $input = $(str);
+            self.$input = $input;
+            self.container.replaceWith($input);
+            self.warP = $input.find('#war1');
+            self.war1Space = $input.find('#war1_space');
+            self.num1 = $input.find('span.num1');
+            self.num2 = $input.find('span.num2');
+            self.textarea = $input.find('textarea');
+        }
+        Textarea.prototype.initStatus = function() {
+            var self = this;
+
+            self.warP.hide();
+        }
+        Textarea.prototype.computeNumber = function() {
+            var self = this;
+            // 执行进不到事件里边，很奇怪。靠，id名拼错
+            self.textarea.on('input', function() {
+                var num = $(this).val().length;
+                var Tnum = self.maxlength;
+                if (num <= 0) {
+                    self.war1Space.show();
+                    self.warP.hide();
+                } else {
+                    self.war1Space.hide();
+                    self.warP.show();
+                    self.num1.text(self.maxlength - num);
+                    self.num2.text(self.maxlength);
+                }
+            })
+        }
+    })();
+    // 地区选择框组件
+    /*(function(){
+        var AreaSelector = TYinput.AreaSelector = function() {
+
+        }
+        AreaSelector.prototype.configuration = {
+            container: ''
+        }
+        AreaSelector.prototype.init = function(options) {
+            var self = this;
+            $.extend(true, this, this.configuration, options);
+        }
+        AreaSelector.prototype.createDom = function(){
+            var self = this;
+            var str = ""
+        }
+    })()*/
+    // 下拉选择组件
+    (function(){
+        
+        /*
+         * 这是第一个应用到数据请求模板和数据来完成dom的组件，遇到的问题如下：
+         * 1、定义数据获取函数，由于要定义回调以及复用性问题，需要在公共工具集里定义
+         * 公共方法。
+         * 2、数据获取转化问题，可能不具有普遍性；这里遇到的问题就是dot模板在外部引用时，
+         * 它返回的是一个匿名函数，不是html。解决办法：看调试器信息，这个匿名函数返回了一个
+         * 加载数据后的字符串，如果没有数据，它返回的是一个空字符串，即给它传递一个数据并让
+         * 它执行，就能得到想要的dom
+         */
+
+        var Selector = TYinput.Selector = function(){
+
+        }
+        Selector.prototype.configuration = {
+            container : '',
+            alias : '',
+            length : 10,
+            labelLength : 2,
+            dataSource : '',       /*安邦里边这里定义了两种形式，一种是数组，另一种是url，这里先写一种*/
+            defaultOption : '',
+            name : ''
+        }
+        Selector.prototype.init = function(options){
+            var self = this;
+            $.extend(true,this,self.configuration,options);
+            self.initConfig();
+            self.createDom();
+        }
+        Selector.prototype.initConfig = function(){
+            var self = this;
+            self.$container = $(self.container);
+
+        }
+        Selector.prototype.createDom = function(){
+            var self = this;
+            var str = '<div class = "form-group">\
+                            <label class = "control-label col-xs-' + self.labelLength + '">' + self.alias + '</label>\
+                            <div class = "col-xs-' + self.length + '">\
+                                <select class = "form-control">\
+                                <option selected>' + self.defaultOption + '</option>\
+                                </select>\
+                            </div>\
+                        </div>'
+            self.$input = $(str);
+            self.$container.replaceWith(self.$input);
+            // 获取模板并渲染数据
+            Utils.requireTmpl('selector',function(tpl){
+                // 模板地址
+                console.log(tpl);
+                var url = self.dataSource;
+                var optiondata;
+                var data,error;
+                // 已经得到了模板，接下来获取数据
+                Utils.getData(url,'',function(response){
+                    console.log('已得到json数据');
+                    if(response.status !== 0) {
+                        console.log('获取下拉框数据失败');
+                        return;
+                    };
+                    optiondata = response.data;
+                    // 已经得到了数据，渲染数据到模板
+                    Utils.render({
+                        tmpl : tpl,
+                        data : optiondata,
+                        container_child : self.$input.find('select')
+                    });
+                })
+            })
+        }
+
+    })()
+    // 暴露到全局
+    window.tyinput = {
+        initText: function(options) {
+            var tyinput = new TYinput.Text();
+            tyinput.init(options);
+            return tyinput;
+        },
+        initTextarea: function(options) {
+            var tytextarea = new TYinput.Textarea();
+            tytextarea.init(options);
+            return tytextarea;
+        },
+        initSelector: function(options){
+            var tyselector = new TYinput.Selector();
+            tyselector.init(options);
+            return tyselector;
+        }
+    };
+})()
---
+++ yun_weixin/src/scripts/components/table.js
@@ -0,0 +1,159 @@
+// 普通写法
+(function() {
+
+    var TYtable = function() {
+
+        }
+        /*
+         * 1、将一个dot模板写到一个组件里边，应该怎么写？安邦里的写法，
+         *模板是一个单独的文件，用ajax请求的方式来来请求模板，中间隔了一层util工具类函数，
+         *进行模板的请求和处理。这里也确实不适合写到组件里边，那样的话组件就不好阅读了，也
+         *不利于组件和模板的维护。
+         * 2、数据请求，通过ajax来请求
+         * 3、在进行数据表格渲染时，涉及到两方面的数据，一个是表头数据，由由用户配置，一个
+         * 是表格体数据，由后台获取，但在渲染时用到的是一个数据进行渲染的，so，要对两个数据
+         * 进行处理，处理成一个数据，在用于渲染。
+         * 4、在chrome里遇到一个问题，即点击浏览器回退按钮后，页面的渲染会丢失组件，并且在调试
+         * 的时候，具有很多的不确定性，此时的代码在火狐和IE下是没问题的；然后：将调用的获取模板
+         * 并渲染的函数放在获取数据的回调里面，问题就解决了
+         * 5、涉及到状态储存的问题，例如：现在设置表格显示20条数据，然后点击编辑跳转页面，在点击
+         * 浏览器的回退按钮返回，如果没有本地存储或者没有用到本地存储，那么表格渲染的时候会渲染10
+         * 条数据，因为它渲染时的状态是初始化时的状态，并不记得select事件选择20条数据的状态，并且
+         * 根据这个状态，好多东西都要更新，还是很复杂的；接下来了解本地存储吧
+         * 
+         */
+    TYtable.prototype.configuration = {
+        container: '',
+        url: '',
+        operation: '',
+        // 表头每一列的信息
+        cols: [{
+            name: 'number',
+            alias: '工号'
+        }, {
+            name: 'name',
+            alias: '姓名'
+        }, {
+            name: 'department',
+            alias: '部门'
+        }, {
+            name: 'role',
+            alias: '工作'
+        }]
+    }
+    TYtable.prototype.init = function(options) {
+            var self = this;
+            $.extend(true, this, self.configuration, options);
+            // 先加载DOM框架
+            self.initFrame();
+            self.delData();
+            self.pagingRender();
+            self.trigger();
+        }
+        // 初始化框架，分为table框架和分页框架
+    TYtable.prototype.initFrame = function() {
+            var self = this;
+            self.$container = $(self.container);
+            var htmlstr = '<div class="table_content">\
+                            <div class="tableContainer"></div>\
+                            <div class="tablePage"></div>\
+                        </div>';
+            self.$input = $(htmlstr);
+            self.$input.appendTo(self.container)
+        }
+        // 处理数据
+    TYtable.prototype.delData = function() {
+            var self = this;
+            self.tabData = {};
+            self.thdata = [],
+                arr = self.cols,
+                data = '',
+                url = self.url;
+            $.each(arr, function(index, val) {
+                self.thdata.push(val.alias);
+            })
+            self.tabData.thData = self.thdata;
+            Utils.getData(url, data, function(response) {
+                var tdData = response.data;
+                self.tabData.tdData = tdData;
+                
+                self.addOperation();
+                self.render();
+            })
+        }
+        // 增加需要操作的数据
+    TYtable.prototype.addOperation = function() {
+            var self = this;
+            self.$ops = self.operation;
+            self.opsData = [];
+            $.each(self.$ops, function(index, val) {
+                var opsStr = '<a href = "javascript:;" class = "operation-' + index + '">' + val.name + '</a>';
+                self.opsData.push(opsStr);
+                self.$container.on('click','.operation-' + index,function(){
+                    // 这里如果直接向下面这样返回，第二次返回的时候就会覆盖第一次返回的内容;
+                    // 循环绑定事件是没有问题的，出错是因为事件代理搞错了，没有绑定到相应的
+                    // 元素上面
+                	val.todo();
+                })
+            })
+            self.tabData.opsData = self.opsData;
+        }
+        // 模板渲染
+    TYtable.prototype.render = function() {
+        var self = this;
+        // 获取模板
+        Utils.requireTmpl('table', function(tpl) {
+        	self.tpl = tpl;
+            Utils.render({
+                data: self.tabData,
+                container_child: self.$input.find('.tableContainer'),
+                tmpl: tpl
+            })
+        })
+    }
+        // 分页
+    TYtable.prototype.pagingRender = function(){
+    	var self = this;
+    	Utils.requireTmpl('page1',function(tpl){
+    		Utils.render({
+    			data : '',
+    			container_child : self.$input.find('.tablePage'),
+    			tmpl : tpl
+    		})
+    	})
+    }
+    TYtable.prototype.trigger = function(){
+    	var self = this;
+    	self.$page = self.$input.find('.tablePage');
+        // 这里我直接通过find方法查找select元素，找不到，先找到page，用事件代理继续绑定才可以
+    	self.$page.on('change','.tytable-select',function(){
+    		var pageNum = 2;
+    		self.pagingRenderAgain(pageNum);
+    	})
+    }
+    TYtable.prototype.pagingRenderAgain = function(n){
+        var self = this;
+        var url = '../apis/table3/table3-' + n + '.json';
+        var data;
+        Utils.getData(url,data,function(rep){
+        	var pageData = {};
+        	pageData.thData = self.thdata;
+        	pageData.tdData = rep.data;
+        	pageData.opsData = self.opsData;
+            Utils.render({
+            	data : pageData,
+            	container_child : self.$input.find('.tableContainer'),
+            	tmpl : self.tpl
+            })
+        })
+    }
+    
+    // 暴露到全局
+    window.tytable = {
+        init: function(options) {
+            var tytable = new TYtable();
+            tytable.init(options);
+            return tytable;
+        }
+    }
+})()
